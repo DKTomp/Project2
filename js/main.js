40a29b1756430ff4c7bef7839fbfc716d79d596d
DKTomp <dylan.tompkins1991@yahoo.com>
2025-12-03 22:44:50 -0600

    second commit

--- js/main.js
+++ js/main.js
@@ -1,20 +1,26 @@
 async function fetchData() {
-    let userInput = document.getElementById("input").value
-    let responseGigphy = await fetch (`https://api.giphy.com/v1/gifs/search?api_key=AbSNfVKiCVf9GMPAnAsNGuG4QlhrnIAo&q=${userInput}&limit=12&offset=0&rating=g&lang=en&bundle=messaging_non_clips`)
-    let dataGiphy = await responseGigphy.json()
+    try {
+        let userInput = document.getElementById("input").value
+        let responseGigphy = await fetch (`https://api.giphy.com/v1/gifs/search?api_key=AbSNfVKiCVf9GMPAnAsNGuG4QlhrnIAo&q=${userInput}&limit=12&offset=0&rating=g&lang=en&bundle=messaging_non_clips`)
+        let dataGiphy = await responseGigphy.json()
 
-    let arrayGigphy = dataGiphy.data
-    console.log(arrayGigphy)
+        let arrayGigphy = dataGiphy.data
+        console.log(arrayGigphy)
 
-    arrayGigphy.forEach(element => {
-        let newGIF = document.createElement ("img")
-        newGIF.src = element.images.original.url 
-        document.getElementById("giphy-container").appendChild(newGIF)   
-    });
+        arrayGigphy.forEach(element => {
+            let newGIF = document.createElement ("img")
+            newGIF.src = element.images.original.url 
+            document.getElementById("giphy-container").appendChild(newGIF)   
+        });
+    } catch (error) {
+        console.log("fetch error")
+    }
 }
 
 function clearData() {
     document.getElementById("giphy-container").textContent = ""
+    document.getElementById("input").value = ""
+    
 }
 
 
